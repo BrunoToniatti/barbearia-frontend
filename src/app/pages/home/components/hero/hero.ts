b7fb5b4d5cc0fad047057f3c966094f5e6ac283a
Bruno Toniatti <toniattibruno08@gmail.com>
2025-08-02 00:29:08 -0300

    Criando projeto

---
+++ src/app/pages/home/components/hero/hero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-hero',
+  imports: [MatIcon],
+  templateUrl: './hero.html',
+  styleUrl: './hero.scss'
+})
+export class Hero {
+
+}
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from "./components/header/header";
+import { Hero } from "./components/hero/hero";
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, Hero],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
